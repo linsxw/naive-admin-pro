7c91316b098de6c632dcd2b51adb5f39e709efe3
linsxw <804636497@qq.com>
2024-04-03 22:58:30 +0800

    feat: 添加自定义主题参数

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,78 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useMessage } from 'naive-ui'
 
+export default defineComponent({
+  setup() {
+    const message = useMessage()
+    return {
+      info() {
+        message.info(
+          'I don\'t know why nobody told you how to unfold your love',
+          {
+            keepAliveOnHover: true,
+          },
+        )
+      },
+      error() {
+        message.error('Once upon a time you dressed so fine')
+      },
+      warning() {
+        message.warning('How many roads must a man walk down')
+      },
+      success() {
+        message.success(
+          '\'Cause you walked hand in hand With another man in my place',
+        )
+      },
+      loading() {
+        message.loading(
+          'If I were you, I will realize that I love you more than any other guy',
+        )
+      },
+    }
+  },
+})
 </script>
 
 <template>
-  <div>
-    首页
-  </div>
+  <n-space>
+    <n-button @click="info">
+      信息（Hover不消失）
+    </n-button>
+    <n-button @click="error">
+      错误
+    </n-button>
+    <n-button @click="warning">
+      警告
+    </n-button>
+    <n-button @click="success">
+      成功
+    </n-button>
+    <n-button @click="loading">
+      加载中
+    </n-button>
+  </n-space>
+
+  <n-space class="mt-2">
+    <n-button>Default</n-button>
+    <n-button type="tertiary">
+      Tertiary
+    </n-button>
+    <n-button type="primary">
+      Primary
+    </n-button>
+    <n-button type="info">
+      Info
+    </n-button>
+    <n-button type="success">
+      Success
+    </n-button>
+    <n-button type="warning">
+      Warning
+    </n-button>
+    <n-button type="error">
+      Error
+    </n-button>
+  </n-space>
 </template>
