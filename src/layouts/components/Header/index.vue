3630d7110621c0e2d5057c437db8ff67aea453ab
linsxw <804636497@qq.com>
2024-04-04 12:58:59 +0800

    feat: 完善布局头部

--- src/layouts/components/Header/index.vue
+++ src/layouts/components/Header/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { Menu } from '@vicons/ionicons5'
-import { UserOutlined } from '@vicons/antd'
+import { Home, Menu } from '@vicons/ionicons5'
+import { FullscreenOutlined, GithubOutlined, SettingOutlined } from '@vicons/antd'
+import ProjectConfig from './projectConfig.vue'
 
 defineProps({
   collapsed: Boolean,
 })
+
+const activeProjectConfig = ref(false)
 
 const avatarOptions = [
   {
@@ -22,30 +25,48 @@ const avatarOptions = [
   <div class="layout-header">
     <div class="layout-header-left">
       <!-- 菜单收起 -->
-      <div
-        class="layout-header-trigger layout-header-trigger-min ml-1"
-        @click="() => $emit('update:collapsed', !collapsed)"
-      >
-        <n-icon size="18">
-          <Menu />
-        </n-icon>
-      </div>
+      <n-button tertiary circle class="layout-header-trigger" @click="() => $emit('update:collapsed', !collapsed)">
+        <template #icon>
+          <n-icon><Menu /></n-icon>
+        </template>
+      </n-button>
+      <!-- 面包屑 -->
+      <n-breadcrumb>
+        <n-breadcrumb-item>
+          <n-icon :component="Home" size="16" /> 首页
+        </n-breadcrumb-item>
+        <n-breadcrumb-item>
+          xx
+        </n-breadcrumb-item>
+      </n-breadcrumb>
     </div>
     <div class="layout-header-right">
-      <!-- 个人中心 -->
-      <div class="layout-header-trigger layout-header-trigger-min">
-        <n-dropdown trigger="hover" :options="avatarOptions">
-          <div class="avatar">
-            <n-avatar round>
-              linsxw
-              <template #icon>
-                <UserOutlined />
-              </template>
-            </n-avatar>
-          </div>
-        </n-dropdown>
-      </div>
+      <n-button tertiary circle class="layout-header-trigger">
+        <template #icon>
+          <n-icon><FullscreenOutlined /></n-icon>
+        </template>
+      </n-button>
+      <n-button tag="a" tertiary circle class="layout-header-trigger" target="_blank" href="https://github.com/linsxw/naive-admin-pro">
+        <template #icon>
+          <n-icon><GithubOutlined /></n-icon>
+        </template>
+      </n-button>
+      <n-button tertiary circle class="layout-header-trigger" @click="activeProjectConfig = true">
+        <template #icon>
+          <n-icon><SettingOutlined /></n-icon>
+        </template>
+      </n-button>
+      <n-divider vertical class="layout-header-trigger" />
+
+      <n-dropdown :options="avatarOptions">
+        <div class="flex cursor-pointer items-center space-x-1">
+          <n-avatar round :size="35" src="https://pic.imgdb.cn/item/634f902116f2c2beb11a3cc7.jpg" />
+          <span>小伟同学.</span>
+        </div>
+      </n-dropdown>
     </div>
+
+    <ProjectConfig v-model="activeProjectConfig" />
   </div>
 </template>
 
@@ -54,7 +75,6 @@ const avatarOptions = [
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0;
   height: 64px;
   box-shadow: 0 1px 4px rgb(0 21 41 / 8%);
   transition: all 0.2s ease-in-out;
@@ -63,52 +83,19 @@ const avatarOptions = [
   &-left {
     display: flex;
     align-items: center;
+    padding-left: 20px;
   }
 
   &-right {
+    padding-right: 20px;
     display: flex;
     align-items: center;
-    margin-right: 20px;
-
-    .avatar {
-      display: flex;
-      align-items: center;
-      height: 64px;
-    }
-
-    > * {
-      cursor: pointer;
-    }
   }
 
-  &-trigger {
-    display: inline-block;
-    width: 64px;
-    height: 64px;
-    text-align: center;
+  .layout-header-trigger {
+    margin: 0px 8px 0;
     cursor: pointer;
-    transition: all 0.2s ease-in-out;
-
-    .n-icon {
-      display: flex;
-      align-items: center;
-      height: 64px;
-      line-height: 64px;
-    }
-
-    &:hover {
-      background: hsla(0, 0%, 100%, 0.08);
-    }
-
-    .anticon {
-      font-size: 16px;
-      color: #515a6e;
-    }
-  }
-
-  &-trigger-min {
-    width: auto;
-    padding: 0 12px;
+    transition: all .2s ease-in-out;
   }
 
   .layout-header-fix {
---
+++ src/layouts/components/Header/projectConfig.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { MoonOutline, SunnyOutline } from '@vicons/ionicons5'
+
+const active = defineModel({ type: Boolean, default: false })
+const configSetting = ref()
+const animateOptions = [
+  {
+    label: '无',
+    value: 'none',
+  },
+  {
+    label: '淡入淡出',
+    value: 'fade',
+  },
+  {
+    label: '滑动',
+    value: 'slide',
+  },
+]
+</script>
+
+<template>
+  <n-drawer v-model:show="active" :width="300" placement="right">
+    <n-drawer-content title="系统配置">
+      <n-divider>主题配置</n-divider>
+      <div class="drawer-config-item justify-center">
+        <n-tabs type="segment" animated>
+          <n-tab-pane name="chap1">
+            <template #tab>
+              <n-icon><SunnyOutline /></n-icon>
+            </template>
+          </n-tab-pane>
+          <n-tab-pane name="chap2">
+            <template #tab>
+              <n-icon><MoonOutline /></n-icon>
+            </template>
+          </n-tab-pane>
+        </n-tabs>
+      </div>
+
+      <n-divider>其他配置</n-divider>
+      <div class="drawer-config-item">
+        <div class="drawer-config-item-title">
+          固定菜单
+        </div>
+        <div class="drawer-config-item-action">
+          <n-switch v-model="configSetting" />
+        </div>
+      </div>
+
+      <div class="drawer-config-item">
+        <div class="drawer-config-item-title">
+          动画类型
+        </div>
+        <div class="drawer-config-item-select">
+          <n-select v-model:value="configSetting" :options="animateOptions" placeholder="选择动画类型" />
+        </div>
+      </div>
+    </n-drawer-content>
+  </n-drawer>
+</template>
+
+<style lang="scss" scoped>
+.drawer-config-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 14px 0;
+  flex-wrap: wrap;
+
+  &-title {
+    flex: 1 1;
+    font-size: 14px;
+  }
+
+  &-action {
+    flex: 0 0 auto;
+  }
+
+  &-select {
+    flex: 1;
+  }
+}
+</style>
