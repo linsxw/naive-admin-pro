2bb2faa594f7efb6dfd46ab262f13fa1e1d1145a
linsxw <804636497@qq.com>
2024-04-17 20:42:23 +0800

    feat: 添加按钮示例

--- src/views/comp/button/index.vue
+++ src/views/comp/button/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+import { CashOutline as CashIcon } from '@vicons/ionicons5'
 </script>
 
 <template>
@@ -26,6 +26,226 @@
             </n-button>
             <n-button type="error">
               Error
+            </n-button>
+          </n-space>
+        </n-card>
+      </n-gi>
+
+      <n-gi>
+        <n-card title="次要按钮">
+          <n-space>
+            <n-button strong secondary>
+              Default
+            </n-button>
+            <n-button strong secondary type="tertiary">
+              Tertiary
+            </n-button>
+            <n-button strong secondary type="primary">
+              Primary
+            </n-button>
+            <n-button strong secondary type="info">
+              Info
+            </n-button>
+            <n-button strong secondary type="success">
+              Success
+            </n-button>
+            <n-button strong secondary type="warning">
+              Warning
+            </n-button>
+            <n-button strong secondary type="error">
+              Error
+            </n-button>
+            <n-button strong secondary round>
+              Default
+            </n-button>
+            <n-button strong secondary round type="primary">
+              Primary
+            </n-button>
+            <n-button strong secondary round type="info">
+              Info
+            </n-button>
+            <n-button strong secondary round type="success">
+              Success
+            </n-button>
+            <n-button strong secondary round type="warning">
+              Warning
+            </n-button>
+            <n-button strong secondary round type="error">
+              Error
+            </n-button>
+            <n-button strong secondary circle>
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button strong secondary circle type="primary">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button strong secondary circle type="info">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button strong secondary circle type="success">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button strong secondary circle type="warning">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button strong secondary circle type="error">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+          </n-space>
+        </n-card>
+      </n-gi>
+
+      <n-gi>
+        <n-card title="次次要按钮">
+          <n-space>
+            <n-button tertiary>
+              Default
+            </n-button>
+            <n-button tertiary type="primary">
+              Primary
+            </n-button>
+            <n-button tertiary type="info">
+              Info
+            </n-button>
+            <n-button tertiary type="success">
+              Success
+            </n-button>
+            <n-button tertiary type="warning">
+              Warning
+            </n-button>
+            <n-button tertiary type="error">
+              Error
+            </n-button>
+            <n-button tertiary round>
+              Default
+            </n-button>
+            <n-button tertiary round type="primary">
+              Primary
+            </n-button>
+            <n-button tertiary round type="info">
+              Info
+            </n-button>
+            <n-button tertiary round type="success">
+              Success
+            </n-button>
+            <n-button tertiary round type="warning">
+              Warning
+            </n-button>
+            <n-button tertiary round type="error">
+              Error
+            </n-button>
+            <n-button tertiary circle>
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button tertiary circle type="primary">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button tertiary circle type="info">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button tertiary circle type="success">
+              <n-icon><CashIcon /></n-icon>
+            </n-button>
+            <n-button tertiary circle type="warning">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button tertiary circle type="error">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+          </n-space>
+        </n-card>
+      </n-gi>
+
+      <n-gi>
+        <n-card title="次次次要按钮">
+          <n-space>
+            <n-button quaternary>
+              Default
+            </n-button>
+            <n-button quaternary type="primary">
+              Primary
+            </n-button>
+            <n-button quaternary type="info">
+              Info
+            </n-button>
+            <n-button quaternary type="success">
+              Success
+            </n-button>
+            <n-button quaternary type="warning">
+              Warning
+            </n-button>
+            <n-button quaternary type="error">
+              Error
+            </n-button>
+            <n-button quaternary round>
+              Default
+            </n-button>
+            <n-button quaternary round type="primary">
+              Primary
+            </n-button>
+            <n-button quaternary round type="info">
+              Info
+            </n-button>
+            <n-button quaternary round type="success">
+              Success
+            </n-button>
+            <n-button quaternary round type="warning">
+              Warning
+            </n-button>
+            <n-button quaternary round type="error">
+              Error
+            </n-button>
+            <n-button quaternary circle>
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button quaternary circle type="primary">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button quaternary circle type="info">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button quaternary circle type="success">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button quaternary circle type="warning">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
+            </n-button>
+            <n-button quaternary circle type="error">
+              <template #icon>
+                <n-icon><CashIcon /></n-icon>
+              </template>
             </n-button>
           </n-space>
         </n-card>
