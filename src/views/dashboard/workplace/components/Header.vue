5e07d46ee2ffd46e999951d4274833bfc3498332
linsxw <804636497@qq.com>
2024-04-21 14:59:42 +0800

    feat: 工作台页面

---
+++ src/views/dashboard/workplace/components/Header.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { UserOutlined } from '@vicons/antd'
+</script>
+
+<template>
+  <div class="items-center justify-between py-2 md:flex">
+    <div class="items-center md:flex">
+      <n-avatar
+        round
+        class="mr-4"
+        :size="50"
+        src="https://pic.imgdb.cn/item/634f902116f2c2beb11a3cc7.jpg"
+      />
+      <div class="flex flex-col">
+        <div class="mb-3 text-[20px]">
+          早安，小伟同学.，开始您一天的工作吧！
+        </div>
+        <n-el tag="div" class="mb-1 flex items-center md:mb-0" :style="{ color: 'var(--text-color-3)' }">
+          今日阴转小雨，22℃ - 32℃，出门记得带伞哦。
+        </n-el>
+      </div>
+    </div>
+    <div class="flex">
+      <n-space :size="30">
+        <div class="flex flex-col items-end">
+          <n-el tag="div" class="mb-1 flex items-center" :style="{ color: 'var(--text-color-3)' }">
+            <n-button strong secondary circle size="tiny" type="primary" class="!mr-1">
+              <template #icon>
+                <n-icon><UserOutlined /></n-icon>
+              </template>
+            </n-button>
+            项目数
+          </n-el>
+          <div class="text-xl">
+            12
+          </div>
+        </div>
+        <div class="flex flex-col items-end">
+          <n-el tag="div" class="mb-1" :style="{ color: 'var(--text-color-3)' }">
+            <n-button strong secondary circle size="tiny" type="warning" class="!mr-1">
+              <template #icon>
+                <n-icon><UserOutlined /></n-icon>
+              </template>
+            </n-button>
+            待办项
+          </n-el>
+          <div class="text-xl">
+            3 / 24
+          </div>
+        </div>
+        <div class="flex flex-col items-end">
+          <n-el tag="div" class="mb-1" :style="{ color: 'var(--text-color-3)' }">
+            <n-button strong secondary circle size="tiny" type="success" class="!mr-1">
+              <template #icon>
+                <n-icon><UserOutlined /></n-icon>
+              </template>
+            </n-button>
+            积分
+          </n-el>
+          <div class="text-xl">
+            1,689
+          </div>
+        </div>
+      </n-space>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/dashboard/workplace/index.vue
+++ src/views/dashboard/workplace/index.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
+import Header from './components/Header.vue'
 import { Container } from '@/components/Container'
 </script>
 
 <template>
   <Container>
-    <n-card>没有标题</n-card>
+    <n-card :bordered="false">
+      <Header />
+    </n-card>
+    <div class="mt-4">
+      <n-grid x-gap="12" :cols="2">
+        <n-gi>
+          <n-card segmented :bordered="false" title="我的项目" size="small">
+            内容
+          </n-card>
+        </n-gi>
+        <n-gi>
+          <n-card segmented :bordered="false" title="快捷操作" size="small">
+            内容
+          </n-card>
+        </n-gi>
+      </n-grid>
+    </div>
   </Container>
 </template>
 
