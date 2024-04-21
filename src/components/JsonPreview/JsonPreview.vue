7bf9a1f1b89379c887810f46d6dce3bd13a2d76e
linsxw <804636497@qq.com>
2024-04-21 14:23:31 +0800

    feat: 添加json预览组件

---
+++ src/components/JsonPreview/JsonPreview.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import VueJsonPretty from 'vue-json-pretty'
+import 'vue-json-pretty/lib/styles.css'
+
+defineProps({
+  data: Object,
+})
+</script>
+
+<template>
+  <VueJsonPretty :deep="3" show-icon :show-length="true" :data="data" />
+</template>
--- src/views/feat/json-preview/index.vue
+++ src/views/feat/json-preview/index.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-
+const jsonStr = ref({
+  userList: [
+    {
+      id: 1,
+      name: '张三',
+    },
+    {
+      id: 2,
+      name: '李四',
+    },
+  ],
+})
 </script>
 
 <template>
-  <div>
-    json预览
-  </div>
+  <Container title="Json预览组件" description="JSON预览编辑">
+    <n-grid x-gap="12" y-gap="12" :cols="2">
+      <n-gi>
+        <n-card title="预览">
+          <JsonPreview :data="jsonStr" />
+        </n-card>
+      </n-gi>
+    </n-grid>
+  </Container>
 </template>
 
 <style scoped lang="scss">
