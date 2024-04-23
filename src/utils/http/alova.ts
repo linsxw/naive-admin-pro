847c5df5aec368f5f72b01c9034e8a9e915cf36a
linsxw <804636497@qq.com>
2024-04-23 23:01:51 +0800

    fix: 删除多余console，自动携带token

--- src/utils/http/alova.ts
+++ src/utils/http/alova.ts
@@ -1,6 +1,7 @@
 import { createAlova } from 'alova'
 import VueHook from 'alova/vue'
 import GlobalFetch from 'alova/GlobalFetch'
+import { useUserStore } from '@/store/modules/user.ts'
 
 export const useHttp = createAlova({
   baseURL: import.meta.env.VITE_BASE_API,
@@ -11,40 +12,40 @@ export const useHttp = createAlova({
 
   /**
    * 执行请求之前的钩子
-   * @param method
    */
   beforeRequest(method) {
-    console.log('beforeRequest', method)
+    const userStore = useUserStore()
+    // 携带token
+    if (userStore.getToken) {
+      method.config.headers.Authorization = `Bearer ${userStore.getToken}`
+    }
   },
 
   responded: {
     /**
      * 请求成功的钩子
-     * @param response
-     * @param method
      */
-    onSuccess: async (response, method) => {
+    onSuccess: async (response) => {
       const json = await response.json()
-      console.log('onSuccess', json, method, response)
       return json.data
     },
 
-    /**
-     * 请求失败的钩子
-     * @param err
-     * @param method
-     */
-    onError: (err, method) => {
-      console.log('onError', err, method)
-    },
-
-    /**
-     * 请求完成的钩子
-     * @param method
-     */
-    onComplete: (method) => {
-      console.log('onComplete', method)
-    },
+    // /**
+    //  * 请求失败的钩子
+    //  * @param err
+    //  * @param method
+    //  */
+    // onError: (err, method) => {
+    //   console.log('onError', err, method)
+    // },
+    //
+    // /**
+    //  * 请求完成的钩子
+    //  * @param method
+    //  */
+    // onComplete: (method) => {
+    //   console.log('onComplete', method)
+    // },
   },
 
   // /**
