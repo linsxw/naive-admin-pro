29be9125ec7fe84d81486804d68e83769dec551e
linsxw <804636497@qq.com>
2024-04-29 13:51:48 +0800

    style: add reset.css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import '@/styles/scss/index.scss'
 import { setupStore } from './store'
