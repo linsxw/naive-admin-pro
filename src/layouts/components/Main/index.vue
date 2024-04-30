bbf9c387f5d9a47699cfb413e1e6a4e2b26c93f3
linsxw <804636497@qq.com>
2024-04-30 20:47:22 +0800

    style: 添加fade页面过度

--- src/layouts/components/Main/index.vue
+++ src/layouts/components/Main/index.vue
@@ -1,5 +1,7 @@
 <template>
   <router-view v-slot="{ Component, route }">
-    <component :is="Component" :key="route.path" />
+    <transition name="fade" mode="out-in">
+      <component :is="Component" :key="route.path" />
+    </transition>
   </router-view>
 </template>
